da8943883f1515a42907981246afb0cc8827f958
Ivan Karpov <idkarpov@yandex.ru>
2023-02-13 22:08:48 +0300

    Тесты для функций get_connection_dsn и is_report_range_valid

--- my_wallet/utils/config.py
+++ my_wallet/utils/config.py
@@ -6,3 +6,14 @@ def get_connection_dsn(config: Mapping[str, Any]) -> str:
         f"postgresql://{config['POSTGRES_USER']}:{config['POSTGRES_PASSWORD']}@"
         f"{config['POSTGRES_HOST']}:{config['POSTGRES_PORT']}/{config['POSTGRES_DBNAME']}"
     )
+
+if __name__ == "__main__":
+    config = {
+        'POSTGRES_USER': 'user',
+        'POSTGRES_PASSWORD': 'qwerty',
+        'POSTGRES_HOST': 'local_host',
+        'POSTGRES_PORT': '5432',
+        'POSTGRES_DBNAME': 'test_db',
+    }
+
+    print(get_connection_dsn(config))
